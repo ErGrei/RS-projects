16b73bd46897097885d91aec0036566fcbe71bb6
ErGrei <i.s.piskunov@gmail.com>
2023-12-02 14:36:25 +0300

    feat: Implemented a burger menu and switching cards to the menu block

--- coffee-house/JS/script.js
+++ coffee-house/JS/script.js
@@ -20,5 +20,32 @@
 // }
 
 /**Бургер меню */
+const btnBoorgerMenu = document.querySelector(".heder-btn__hover");
+const navbarBoorgerMenu = document.querySelector(".navbar");
+const navbarLinkMenu = document.querySelectorAll(".menu-link");
 
-
+btnBoorgerMenu.addEventListener("click", () => {
+  navbarBoorgerMenu.classList.toggle("navbar-ativ");
+});
+for (let i = 0; i < navbarLinkMenu.length; i++) {
+  navbarLinkMenu[i].addEventListener("click", () => {
+    console.log(1);
+    navbarBoorgerMenu.classList.toggle("navbar-ativ");
+  });
+}
+/**Переключение карточек в блоке menu */
+const btnMenu = document.querySelectorAll(".btn-item");
+const cartMenu = document.querySelectorAll(".contanier-menu");
+const btnMenuImg = document.querySelectorAll(".menu-btn_img");
+for (let i = 0; i < btnMenu.length; i++) {
+  btnMenu[i].addEventListener("click", () => {
+    for (let i = 0; i < cartMenu.length; i++) {
+      cartMenu[i].classList.remove("contanier-menu_activ");
+      btnMenu[i].classList.remove("btn-item-active");
+      btnMenuImg[i].classList.remove("menu-btn_img_active");
+    }
+    cartMenu[i].classList.toggle("contanier-menu_activ");
+    btnMenu[i].classList.toggle("btn-item-active");
+    btnMenuImg[i].classList.toggle("menu-btn_img_active");
+  });
+}
