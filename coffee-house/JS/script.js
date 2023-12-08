35834f70cc32c5e98dc2b46abcccfcb290604d26
ErGrei <i.s.piskunov@gmail.com>
2023-12-08 15:49:22 +0300

    feat: added a slider on the main page

--- coffee-house/JS/script.js
+++ coffee-house/JS/script.js
@@ -1,3 +1,4 @@
+/**Popap  в блоке меню */
 const cardMenu = document.querySelectorAll(".cart-menu");
 import data from "./menu.json" assert { type: "json" };
 const cartNumber = data;
@@ -144,25 +145,21 @@ function popapSizeBtnActive(price, sizePrice) {
         Number(popapPrise).toFixed(2);
     });
   }
-  console.log(popapPrise)
+  console.log(popapPrise);
 }
 function popapAdditivesBtnActive() {
   const popapAdditivesBTN = document.querySelectorAll(".popap-Additives-btn");
   const popapAdditives = document.querySelectorAll(".popap-Additives");
-  let AdditivesPrice
+  let AdditivesPrice;
   for (let i = 0; i < popapAdditivesBTN.length; i++) {
     popapAdditivesBTN[i].addEventListener("click", () => {
       popapAdditivesBTN[i].classList.toggle("popap-Additives-btn-active");
       popapAdditives[i].classList.toggle("popap-Additives-active");
-     
     });
   }
 }
 
 
-// document.querySelector(".popap-vraper").addEventListener("click", (e) => {
-
-//   a.removeChild(a.lastChild)})
 
 /**Бургер меню */
 const btnBoorgerMenu = document.querySelector(".heder-btn__hover");
@@ -197,33 +194,77 @@ for (let i = 0; i < btnMenu.length; i++) {
   });
 }
 
-/**Popap  в блоке меню */
 
-// const popapVraper = document.querySelectorAll(".popap-vraper");
-// const popapClose = document.querySelector(".popap-close");
-// const popapSizeBTN = document.querySelectorAll(".popap-size-btn");
-// const popapSize = document.querySelectorAll(".popap-size");
-// const popapAdditivesBTN = document.querySelectorAll(".popap-Additives-btn");
-// const popapAdditives = document.querySelectorAll(".popap-Additives");
-// // let popapPrise = document.querySelector(".popap-prise").innerHTML;
 
-// const prise = [0, 0.5, 1];
+/**Слайдер в блоке Home */
+window.addEventListener("resize", slideAdaptiv);
+const timeAutSlider = document.querySelector(".Favourites-Coffee");
+const sliderCart = document.querySelectorAll(".sleder-cart");
+const sliderContener = document.querySelector(".Favourites-Coffee__contener");
+const sliderBtnRight = document.querySelector(
+  ".Favourites-Coffee__slider_right"
+);
+const sliderBtnLeft = document.querySelector(".Favourites-Coffee__slider_left");
+const sliderline = document.querySelector(".slider-line");
+const slierDot = document.querySelectorAll(".sler-dot");
 
-// for (let i = 0; i < popapSizeBTN.length; i++) {
-//   popapSizeBTN[i].addEventListener("click", () => {
-//     for (let i = 0; i < popapSizeBTN.length; i++) {
-//       if (popapSizeBTN[i].classList.contains("popap-size-btn-active")) {
-//         popapPrise = +popapPrise - prise[i];
-//       }
-//       popapSizeBTN[i].classList.remove("popap-size-btn-active");
-//       popapSize[i].classList.remove("popap-size-active");
-//     }
-//     popapSizeBTN[i].classList.toggle("popap-size-btn-active");
-//     popapSize[i].classList.toggle("popap-size-active");
-//     if (popapSizeBTN[i].classList.contains("popap-size-btn-active")) {
-//       popapPrise = +popapPrise + prise[i];
-//     }
-//     document.querySelector(".popap-prise").innerHTML =
-//       Number(popapPrise).toFixed(2);
-//   });
-// }
+sliderBtnRight.addEventListener("click", sliderRight);
+sliderBtnLeft.addEventListener("click", sliderLeft);
+
+let sliderCount = 0;
+let sliderWidth;
+function slideAdaptiv() {
+  sliderWidth = sliderline.offsetWidth;
+  sliderline.style.width = sliderWidth * sliderCart.length + "px";
+  sliderCart.forEach((item) => (item.style.width = sliderWidth + "px"));
+  rollSlider();
+}
+slideAdaptiv();
+function sliderRight() {
+  sliderCount++;
+  if (sliderCount >= sliderCart.length) {
+    sliderCount = 0;
+  }
+  rollSlider();
+  slerDotActive(sliderCount);
+}
+function sliderLeft() {
+  sliderCount--;
+  if (sliderCount < 0) {
+    sliderCount = sliderCart.length - 1;
+  }
+  rollSlider();
+  slerDotActive(sliderCount);
+}
+
+function rollSlider() {
+  sliderContener.style.transform = `translateX(${
+    -sliderCount * sliderWidth
+  }px)`;
+}
+
+function slerDotActive(index) {
+  slierDot.forEach((item) => item.classList.remove("sler-dot_active"));
+  slierDot[index].classList.add("sler-dot_active");
+}
+
+let isPaused = false;
+ 
+setInterval(()=>{
+	if(!isPaused){
+    sliderRight();
+	}
+}, 4000);
+ 
+timeAutSlider.addEventListener("mouseenter", function(){
+	if(!isPaused){
+		isPaused = true;
+	} 
+});
+
+timeAutSlider.addEventListener("mouseleave", function(){
+	if(isPaused){
+		isPaused = false;
+	} 
+});
+
