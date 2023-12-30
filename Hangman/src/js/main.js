fc97bf1df6a5022e6438ed013a6fd7041d2751f5
ErGrei <i.s.piskunov@gmail.com>
2023-12-30 05:34:10 +0300

    feat:game logic function has been added, game process output to the page has been implemented

--- Hangman/src/js/main.js
+++ Hangman/src/js/main.js
@@ -7,7 +7,7 @@ const keyboard = [
 let key = [];
 const input = document.createElement("input");
 input.className = "input";
-input.type ="text";
+input.type = "text";
 input.value = "";
 document.body.append(input);
 
@@ -15,21 +15,57 @@ const div = document.createElement("div");
 div.className = "conteiner";
 document.body.append(div);
 
-document.addEventListener('keydown', function(event) {
+document.addEventListener("keydown", function (event) {
   key.push(event.key);
-  input.value = key
-  }
-);
+  input.value = key;
+});
 
 function init() {
   let aut = "";
   for (let i = 0; i < keyboard.length; i++) {
-    aut += '<div class="key" data="'+String.fromCharCode(keyboard[i])+'">' + String.fromCharCode(keyboard[i]) + '</div>';
+    aut += '<div class="key">' + String.fromCharCode(keyboard[i]) + "</div>";
   }
-  document.querySelector('.conteiner').innerHTML = aut;
+  document.querySelector(".conteiner").innerHTML = aut;
 }
-init()
-const cont = document.querySelector('.conteiner');
+init();
+
+const cont = document.querySelector(".conteiner");
 cont.addEventListener("click", (e) => {
-  input.value = e.data.value
-})
+  if (!e.target.classList.contains("key")) return;
+  let item = e.target.innerHTML;
+  openLettersAndNotify(item);
+  console.log(item);
+});
+
+//Игра
+const words = [
+  "апривет",
+  "бпривет",
+  "впривет",
+  "гпривет",
+  "дпривет",
+  "епривет",
+  "жпривет",
+  "зпривет",
+];
+const word = words[Math.floor(Math.random() * words.length)];
+console.log(word);
+
+let answerArray = [];
+for (let i = 0; i < word.length; i++) {
+  answerArray[i] = "_";
+}
+input.value = answerArray.join(" ");
+let remainingLetters = word.length;
+
+function openLettersAndNotify(item) {
+  let guess = item.toLowerCase();
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] === guess) {
+      answerArray[i] = guess;
+      remainingLetters--;
+      input.value = answerArray.join(" ");
+    }
+  }
+}
+
